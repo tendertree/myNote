08c0eb205c4eb4d1ab594d58bfb25e861f7ec9fc
tendertree <tendertree@gmail.com>
2024-03-22 10:15:59 +0900

    feat: add some binaray implements

--- python/src/binary.py
+++ python/src/binary.py
@@ -13,9 +13,22 @@ def binary_search(arr: List[int], target: int) -> int:
             right = mid - 1
     return -1
 
+def firstOccurence(arr:List[int],target:int)->int:
+        l,r=0,len(arr)-1
+        ans= -1
+        while l<=r:
+            mid = (l+r)
+            if arr[mid] == target:
+                ans=mid
+                r=mid-1
+            elif arr[mid]<target:
+                l=mid+1
+            else:
+                r=mid-1
+    
 
-if __name__ == "__main__":
+ __name__ == "__main__":
     arr = [int(x) for x in input().split()]
     target = int(input())
     res = binary_search(arr, target)
-    print(res)
+ print(res)
