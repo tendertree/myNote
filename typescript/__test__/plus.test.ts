efb4ca6a3619fb26c98e2d9439ff6071ccb612dc
tendertree <tendertree@gmail.com>
2024-04-05 11:23:50 +0900

    chore: add some code

--- typescript/__test__/plus.test.ts
+++ typescript/__test__/plus.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest'
 import AddPlusOne from '@/plus'
 test('adds 1 + 2 to equal 3', () => {
-    expect(AddPlusOne(3)).toBe(3)
+	expect(AddPlusOne(5)).toBe(3)
 })
 
 
