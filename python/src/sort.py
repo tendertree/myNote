6ee2e54899be33fffd0051ebb8fb45676fcfb69d
tendertree <tendertree@gmail.com>
2024-04-21 22:02:55 +0900

    feat: add sort examples

--- python/src/sort.py
+++ python/src/sort.py
@@ -7,6 +7,29 @@ def insertion(unsorted_list:List[int])->List[int]:
             current -= 1
         return unsorted_list
 
+def merge(unsorted_list:List[int])->List[int]:
+    n = len(unsorted_list)
+    if n<=1:
+        return unsorted_list
+    mid = n//2
+    left = unsorted_list[:mid]
+    right = unsorted_list[mid:]
+    merge(left)
+    merge(right)
+    i = j = k = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            unsorted_list[k] = left[i]
+            i += 1
+        else:
+            unsorted_list[k] = right[j]
+            j += 1
+        k += 1
+    while i < len(left):
+        unsorted_list[k] = left[i]
+        i += 1
+        k += 1
+
 if __name__ == '__main__':
     unsorted_list = [3, 1, 4, 1, 5, 9, 2, 6]  # Example unsorted list
     sorted_list = insertion(unsorted_list)
