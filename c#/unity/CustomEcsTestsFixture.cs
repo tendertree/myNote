3ecf935f1bbb67d7e9994214c60ce6616cdc61a4
tendertree <tendertree@gmail.com>
2024-07-03 13:38:45 +0900

    feat: add custom test for ecs

---
+++ c#/unity/CustomEcsTestsFixture.cs
@@ -0,0 +1,34 @@
+using Unity.Entities;
+using Unity.Entities.Tests;
+
+namespace Roguelite.Tests.Infrastructure
+{
+    public class CustomEcsTestsFixture : ECSTestsFixture
+    {
+        private const int EntityCount = 1000;
+
+        protected EntityManager Manager => m_Manager;
+
+        protected void UpdateSystem<T>() where T : unmanaged, ISystem
+        {
+            World.GetExistingSystem<T>().Update(World.Unmanaged);
+        }
+
+        protected SystemHandle CreateSystem<T>() where T : unmanaged, ISystem => World.CreateSystem<T>();
+
+        protected Entity CreateEntity(params ComponentType[] types) => Manager.CreateEntity(types);
+
+        protected void CreateEntities(ComponentType[] types, int entityCount = EntityCount)
+        {
+            for (var i = 0; i < entityCount; i++)
+            {
+                Manager.CreateEntity(types);
+            }
+        }
+
+        protected void CreateEntityCommandBufferSystem()
+        {
+            World.CreateSystem<EndSimulationEntityCommandBufferSystem>();
+        }
+    }
+}
